55ba55371930cd899f080c0214e6f7f9821daf7f
Yan Naing Htwe <ynainghtwe61@gmail.com>
2024-10-19 17:47:38 +0900

    Spring boot Testing

--- lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
+++ lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
@@ -2,7 +2,12 @@ package accounts.client;
 
 import common.money.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import rewards.internal.account.Account;
@@ -26,44 +31,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 // TODO-01: Make this class a Spring Boot test class
 // - Add @SpringBootTest annotation with WebEnvironment.RANDOM_PORT
-
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AccountClientTests {
 
 	// TODO-02: Autowire TestRestTemplate bean to a field
 	// - Name the field as restTemplate
 
-	// TODO-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
+	// TODO-03: Update code below to use TestRestTemplate (as opposed to
+	// RestTemplate)
 	// - Remove RestTemplate from this code
 	// - Remove BASE_URL from this code or change the value of it to ""
 	// - Run the tests and observe that they pass except
-	//   "addAndDeleteBeneficiary" test
-	//   (If you are using Gradle, remove test exclude statement
-	//    from the build.gradle before running these tests)
+	// "addAndDeleteBeneficiary" test
+	// (If you are using Gradle, remove test exclude statement
+	// from the build.gradle before running these tests)
 
 	/**
 	 * server URL ending with the servlet mapping on which the application can be
 	 * reached.
 	 */
-	private static final String BASE_URL = "http://localhost:8080";
-
-	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private TestRestTemplate restTemplate = new TestRestTemplate();
 	private Random random = new Random();
 
 	@Test
 	public void listAccounts() {
-		String url = BASE_URL + "/accounts";
+		String url = "/accounts";
 		// we have to use Account[] instead of List<Account>, or Jackson won't know what
 		// type to unmarshal to
 		Account[] accounts = restTemplate.getForObject(url, Account[].class);
 		assertThat(accounts.length >= 21).isTrue();
 		assertThat(accounts[0].getName()).isEqualTo("Keith and Keri Donald");
 		assertThat(accounts[0].getBeneficiaries().size()).isEqualTo(2);
-		assertThat(accounts[0].getBeneficiary("Annabelle").getAllocationPercentage()).isEqualTo(Percentage.valueOf("50%"));
+		assertThat(accounts[0].getBeneficiary("Annabelle").getAllocationPercentage())
+				.isEqualTo(Percentage.valueOf("50%"));
 	}
 
 	@Test
 	public void getAccount() {
-		String url = BASE_URL + "/accounts/{accountId}";
+		String url = "/accounts/{accountId}";
 		Account account = restTemplate.getForObject(url, Account.class, 0);
 		assertThat(account.getName()).isEqualTo("Keith and Keri Donald");
 		assertThat(account.getBeneficiaries().size()).isEqualTo(2);
@@ -72,7 +78,7 @@ public class AccountClientTests {
 
 	@Test
 	public void createAccount() {
-		String url = BASE_URL + "/accounts";
+		String url = "/accounts";
 		// use a random account number to avoid conflict
 		String number = String.format("12345%4d", random.nextInt(10000));
 		Account account = new Account(number, "John Doe");
@@ -90,35 +96,32 @@ public class AccountClientTests {
 	}
 
 	// TODO-04: Modify the code below so that it handles 404 HTTP response status
-	//          from the server (instead of handling it as an exception as in the
-	//          case of RestTemplate)
+	// from the server (instead of handling it as an exception as in the
+	// case of RestTemplate)
 	// - Remove the "assertThrows" statement (since you are not going to
-	//   check if an exception is thrown)
+	// check if an exception is thrown)
 	// - Use "getForEntity" method (instead of "getForObject" method) of
-	//   "TestRestTemplate"
+	// "TestRestTemplate"
 	// - Verify that the HTTP response status is 404
 	// - Run all tests - they should all pass
 	@Test
 	public void addAndDeleteBeneficiary() {
 		// perform both add and delete to avoid issues with side effects
-		String addUrl = BASE_URL + "/accounts/{accountId}/beneficiaries";
+		String addUrl = "/accounts/{accountId}/beneficiaries";
 		URI newBeneficiaryLocation = restTemplate.postForLocation(addUrl, "David", 1);
 		Beneficiary newBeneficiary = restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
 		assertThat(newBeneficiary.getName()).isEqualTo("David");
 
 		restTemplate.delete(newBeneficiaryLocation);
 
-		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
-			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-			restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
-		});
-		assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		ResponseEntity<Beneficiary> response = restTemplate.getForEntity(newBeneficiaryLocation, Beneficiary.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 	// TODO-05: Observe a log message in the console indicating
-	//          Tomcat started as part of testing
+	// Tomcat started as part of testing
 	// - Search for "Tomcat started on port(s):"
 	// - Note how long it takes for this test to finish - it is
-	//   in the range of several seconds
+	// in the range of several seconds
 
 }
--- lab/40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
+++ lab/40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
@@ -1,24 +1,41 @@
 package accounts.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import accounts.AccountManager;
+import rewards.internal.account.Account;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 // TODO-06: Get yourself familiarized with various testing utility classes
 // - Uncomment the import statements below
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.*;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 // TODO-07: Replace @ExtendWith(SpringExtension.class) with the following annotation
 // - @WebMvcTest(AccountController.class) // includes @ExtendWith(SpringExtension.class)
-@ExtendWith(SpringExtension.class)
+@WebMvcTest(AccountController.class)
 public class AccountControllerBootTests {
 
 	// TODO-08: Autowire MockMvc bean
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	AccountManager accountManager;
 
 	// TODO-09: Create AccountManager mock bean using @MockBean annotation
 
@@ -27,16 +44,13 @@ public class AccountControllerBootTests {
 	@Test
 	public void accountDetails() throws Exception {
 
-		//given(accountManager.getAccount(0L))
-		//		.willReturn(new Account("1234567890", "John Doe"));
+		given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 
-		//mockMvc.perform(get("/accounts/0"))
-		//	   .andExpect(status().isOk())
-		//	   .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-		//	   .andExpect(jsonPath("name").value("John Doe"))
-		//	   .andExpect(jsonPath("number").value("1234567890"));
+		mockMvc.perform(get("/accounts/0")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("name").value("John Doe")).andExpect(jsonPath("number").value("1234567890"));
 
-		//verify(accountManager).getAccount(0L);
+		verify(accountManager).getAccount(0L);
 
 	}
 
@@ -47,45 +61,62 @@ public class AccountControllerBootTests {
 	@Test
 	public void accountDetailsFail() throws Exception {
 
-		//given(accountManager.getAccount(any(Long.class)))
-		//		.willThrow(new IllegalArgumentException("No such account with id " + 0L));
+		given(accountManager.getAccount(any(Long.class)))
+				.willThrow(new IllegalArgumentException("No such account with id " + 0L));
 
+		mockMvc.perform(get("/accounts/9999")).andExpect(status().isNotFound());
 		// (Write code here)
 		// - Use mockMvc to perform HTTP Get operation using "/accounts/9999"
-        //   as a non-existent account URL
+		// as a non-existent account URL
 		// - Verify that the HTTP response status is 404
 
-		//verify(accountManager).getAccount(any(Long.class));
+		verify(accountManager).getAccount(any(Long.class));
 
 	}
 
-    // TODO-12: Write test for `POST` request for an account
+	// TODO-12: Write test for `POST` request for an account
 	// - Uncomment Java code below
 	// - Write code between the "given" and "verify" statements
 	// - Run the test and verify it succeeds
 	@Test
 	public void createAccount() throws Exception {
 
-		//Account testAccount = new Account("1234512345", "Mary Jones");
-		//testAccount.setEntityId(21L);
+		Account testAccount = new Account("1234512345", "Mary Jones");
+		testAccount.setEntityId(21L);
 
-		//given(accountManager.save(any(Account.class)))
-		//		.willReturn(testAccount);
+		given(accountManager.save(any(Account.class))).willReturn(testAccount);
 
 		// (Write code here)
 		// Use mockMvc to perform HTTP Post operation to "/accounts"
 		// - Set the request content type to APPLICATION_JSON
 		// - Set the request content with Json string of the "testAccount"
-		//   (Use "asJsonString" method below to convert the "testAccount"
-		//   object into Json string)
+		// (Use "asJsonString" method below to convert the "testAccount"
+		// object into Json string)
 		// - Verify that the response status is 201
-		// - Verify that the response "Location" header contains "http://localhost/accounts/21"
+		// - Verify that the response "Location" header contains
+		// "http://localhost/accounts/21"
+		mockMvc.perform(post("/accounts").contentType(MediaType.APPLICATION_JSON).content(asJsonString(testAccount)))
+				.andExpect(status().isCreated()).andExpect(header().string("location", "http://localhost/accounts/21"));
 
-		//verify(accountManager).save(any(Account.class));
+		verify(accountManager).save(any(Account.class));
 
 	}
 
-    // Utility class for converting an object into JSON string
+	@Test
+	public void accountSummary() throws Exception {
+
+		List<Account> testAccounts = Arrays.asList(new Account("123456789", "John Doe"));
+		given(accountManager.getAllAccounts()).willReturn(testAccounts);
+
+		mockMvc.perform(get("/accounts")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$..number").value("123456789")).andExpect(jsonPath("$..name").value("John Doe"));
+
+		verify(accountManager).getAllAccounts();
+
+	}
+
+	// Utility class for converting an object into JSON string
 	protected static String asJsonString(final Object obj) {
 		try {
 			final ObjectMapper mapper = new ObjectMapper();
